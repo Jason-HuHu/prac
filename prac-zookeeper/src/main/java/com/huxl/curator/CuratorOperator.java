b55a8b98a36f528b1ab2e906cad8f85e9c519d86
huxl <1259450641@qq.com>
2019-05-22 17:58:55 +0800

    add zk

---
+++ prac-zookeeper/src/main/java/com/huxl/curator/CuratorOperator.java
@@ -0,0 +1,39 @@
+package com.huxl.curator;
+
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+
+/**
+ * Curator基本操作
+ *
+ * @author huxl
+ * @since 2019/5/22 17:48
+ */
+public class CuratorOperator {
+
+    private CuratorFramework client;
+
+    private static final String ZK_SERVER_PATH = "192.168.99.100:2181";
+
+    private static final int SESSION_TIMEOUT = 60*1000;
+
+    public static void main(String[] args) {
+
+    }
+
+    private void init(){
+
+        /*
+            curator 连接zk的策略
+         */
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000,5);
+
+        client = CuratorFrameworkFactory.builder()
+                .connectString(ZK_SERVER_PATH)
+                .sessionTimeoutMs(SESSION_TIMEOUT).retryPolicy(retryPolicy)
+                .build();
+    }
+
+}

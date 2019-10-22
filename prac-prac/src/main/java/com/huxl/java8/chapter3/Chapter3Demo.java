fafb06164163b2754e145e68c4cc5df532008706
huxl <1259450641@qq.com>
2019-10-22 14:10:38 +0800

    add java8实战 Lambda表达式

--- prac-prac/src/main/java/com/huxl/java8/chapter3/Chapter3Demo.java
+++ prac-prac/src/main/java/com/huxl/java8/chapter3/Chapter3Demo.java
@@ -5,11 +5,9 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.IntPredicate;
-import java.util.function.Predicate;
+import java.util.function.*;
 
 public class Chapter3Demo {
     public static void main(String[] args) throws IOException {
@@ -40,12 +38,12 @@ public class Chapter3Demo {
         process(() -> System.out.println("H"));
 
         //正常读取一行:
-        String oneLine = processFile();
+      /*  String oneLine = processFile();
         System.out.println(oneLine);
         oneLine = processFile((br) -> br.readLine());
         System.out.println(oneLine);
         String twoLine = processFile((br)->br.readLine() + br.readLine());
-        System.out.println(twoLine);
+        System.out.println(twoLine);*/
 
         //抽象方法的签名可以描述Lambda表达式的签名。函数式接口的抽象方法的签名称为函数描述符
 
@@ -75,10 +73,62 @@ public class Chapter3Demo {
             比较两个对象 | (Apple a1,Apple a2) -> a1.getWeight().compareTo(a2.getWeight()) | Comparator<Apple>或者BiFunction<Apple,Apple,Integer>或ToBiFunction<Apple,Apple>
          */
 
-        //!!!!!!!!!!!!!!!!!
+
 
         //任何函数式接口都不允许抛出受检查异常(checked Exception) 如果需要lambda表达式来抛出异常1：自定义一个函数式接口，并声明受检查异常，或者把lambda包在try/cache中
+        System.out.println("类型检查、类型推断及限制");
+        System.out.println("Lambda表达式的类型是从Lambda表达式的上下文推断出来的：接受Lambda表达式的方法的参数，接受它值的局部变量等");
+        /*
+            1.找到方法声明
+            2.目标类型
+            3.找到函数式接口的抽象方法
+            4.抽象方法的函数描述符
+            5.匹配
+         */
+        //同一个Lambda表达式可以用于多个不同的函数接口中
+        Runnable r = ()->{
+            System.out.println("");
+        };
+        r.run();
+        //类型推断：Java编译器会从上下文（目标类型）推断出用什么函数式接口来配合Lambda表达式
+        //Lambda表达式使用局部变量
+        //局部变量必须是final的或者最终是final的，也就是不能再次赋值
+        final int portNumber = 1;
+        Runnable r3 = () -> System.out.println(portNumber);
+        r3.run();
+        //为什么局部变量有一些限制：1.实例变量存储在堆上，而局部变量存储在栈上2.不鼓励使用改变外部变量的典型命令式编程方式
+        //闭包：闭包就是一个函数的实例，而且它可以无限制地访问那个函数的非本地变量，例如闭包可以作为参数传递给另一个函数，可以访问
+        //和修改其作用域之外的变量，现在Java8的Lambda表达式和匿名类可以做类似的事情：他们可以作为参数传递给方法，可以访问其作用域之外的变量
+        //有一个限制：不能修改Lambda表达式的方法的局部变量的内容，这些变量必须是隐式最终的(final)
+        //Lambda表达式对值封闭，但是对变量不封闭
 
+        //方法引用System.out::println 目标引用在::前，方法名称在后，不需要括号，没有真正的调用
+        System.out.println("如何构建方法引用？");
+        //1.指向静态方法的方法引用 Integer::parseInt
+        //2.指向任意类型实例方法的方法引用String::length (String s) -> s.toUpperCase() => String::toUpperCase
+        //3.指向现有对象的实例方法的方法引用 () ->expensiveTransaction.getValue() expensiveTransaction::getValue
+        BiPredicate<List<String>,String> contains = List::contains;
+
+        //构造方法引用
+
+        //实战 给一个Apple列表用不同规则排序 ：行为参数化、匿名类、Lambda表达式和方法引用
+        List<Apple> apples =  map(Arrays.asList(5,6,1,2,3),Apple::new);
+        sortApple(apples);
+        //复合Lambda表达式
+        Comparator<Apple> c = Comparator.comparing(Apple::getWeight);
+        //逆序
+        apples.sort(c.reversed());
+        //比较器链,先比较重量，再比较颜色等等
+        apples.sort(c.reversed().thenComparing(Apple::getColor));
+        //谓词复合negate and  or
+        //函数复合
+        Function<Integer,Integer> f = x -> x + 1;
+        Function<Integer,Integer> g = x -> x * 2;
+
+        Function<Integer,Integer> h = f.andThen(g);
+        Function<Integer,Integer> h1 = f.compose(g);
+        //结果为 4 g(f(x))
+        int re = h.apply(1);
     }
 
     private static void process(Runnable r) {
@@ -114,5 +164,81 @@ public class Chapter3Demo {
             result.add(f.apply(t));
         }
         return result;
+    }
+
+    private static void constructorMethodReffer(){
+        Supplier<Apple> a = Apple::new;
+        Apple apple = a.get();
+
+        Function<Integer,Apple> a1 = Apple::new;
+        Apple apple1 = a1.apply(100);
+
+        //创建不同重量苹果集合
+        List<Apple> apples = map(Arrays.asList(1,2,3),Apple::new);
+
+        BiFunction<String,Integer,Apple> a3 = Apple::new;
+        Apple apple2 = a3.apply("red",110);
+
+    }
+
+    private static void sortApple(List<Apple> oriList) {
+        List<Apple> result = new ArrayList<>();
+        //1.实现接口，传递对象
+        class AppleComparator implements Comparator<Apple>{
+
+            @Override
+            public int compare(Apple o1, Apple o2) {
+                return new Integer(o1.getWeight()).compareTo(new Integer(o2.getWeight()));
+            }
+        }
+
+        oriList.sort(new AppleComparator());
+        //2.使用匿名内部类
+        oriList.sort(new Comparator<Apple>() {
+            @Override
+            public int compare(Apple o1, Apple o2) {
+                return new Integer(o1.getWeight()).compareTo(new Integer(o2.getWeight()));
+            }
+        });
+        //3.Lambda表达式
+        oriList.sort((Apple a1,Apple a2) -> new Integer(a1.getWeight()).compareTo(new Integer(a2.getWeight())));
+        //4.省略参数类型
+        oriList.sort((a1,a2) -> new Integer(a1.getWeight()).compareTo(new Integer(a2.getWeight())));
+        //5.使用静态辅助方法
+        oriList.sort(Comparator.comparing(a -> a.getWeight()));
+        //6.凡使用方法引用
+        oriList.sort(Comparator.comparing(Apple::getWeight));
+    }
+}
+
+class Apple{
+    private int weight;
+    private String color;
+
+    public Apple(){}
+
+    public Apple(int weight) {
+        this.weight = weight;
+    }
+
+    public Apple(String color,int weight) {
+        this.color = color;
+        this.weight = weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
     }
 }

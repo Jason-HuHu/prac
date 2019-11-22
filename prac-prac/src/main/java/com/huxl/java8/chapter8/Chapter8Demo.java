391a1fd52a3fc1be0e828ac8f25d60e2b912d3dc
huxl <1259450641@qq.com>
2019-11-22 21:02:37 +0800

    add 自定义收集器并总结

---
+++ prac-prac/src/main/java/com/huxl/java8/chapter8/Chapter8Demo.java
@@ -0,0 +1,18 @@
+package com.huxl.java8.chapter8;
+
+/**
+ * 使用Lambda表达式重构代码
+ * @author huxl
+ * @since 2019/11/22-20:34
+ */
+public class Chapter8Demo {
+    public static void main(String[] args) {
+        /*
+            策略模式
+            模板方法
+            观察者模式
+            责任链模式
+            工厂模式
+         */
+    }
+}
